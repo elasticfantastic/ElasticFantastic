e777f6e95442e8f8230bfc3f121591c010b85f86
Daniel Nilsson <daniel.nilsson94@outlook.com>
2019-02-21 20:35:29 +0100

    Reworking Docker structure

--- scripts/start_loggenerator.bat
+++ scripts/start_loggenerator.bat
@@ -8,15 +8,15 @@ REM Cd to root of Elasticfantastic
 
 cd ../
 
-if not exist apps\LogGenerator_system mkdir apps\LogGenerator_system
+REM if not exist apps\LogGenerator_system mkdir apps\LogGenerator_system
 
-xcopy "apps\LogGenerator\loggenerator-server\target\loggenerator-server-%VERSION%.jar" "apps\LogGenerator_system" /Y
-xcopy "apps\LogGenerator\loggenerator-client\target\loggenerator-client-%VERSION%.jar" "apps\LogGenerator_system" /Y
+xcopy "apps\LogGenerator\loggenerator-server\target\loggenerator-server-%VERSION%.jar" "." /Y
+xcopy "apps\LogGenerator\loggenerator-client\target\loggenerator-client-%VERSION%.jar" "." /Y
 
-cd apps/LogGenerator_system
+REM cd apps/LogGenerator_system
 
 start cmd /k "java -jar loggenerator-server-%VERSION%.jar"
 timeout /t 10 /nobreak > NUL
 start cmd /k "java -jar loggenerator-client-%VERSION%.jar"
 
-cd ../../scripts
+REM cd ../../scripts
