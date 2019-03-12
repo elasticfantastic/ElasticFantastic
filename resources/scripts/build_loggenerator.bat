16cdf570f8e4226e88b45443c8018a08ffc3c3a3
Daniel Nilsson <daniel.nilsson94@outlook.com>
2019-03-12 13:56:31 +0100

    Improved build and startup process for LogGenerator

--- resources/scripts/build_loggenerator.bat
+++ resources/scripts/build_loggenerator.bat
@@ -2,8 +2,22 @@
 
 REM LogGenerator
 
+SET VERSION=1.0.0-SNAPSHOT
+
 cd ../../apps/LogGenerator/
-mvn -V clean install
+call mvn -V clean install
+
+REM Cd to root of Elasticfantastic
+
+cd ../../
+
+REM Copy compiled binaries from compile directory to root
+
+echo\
+xcopy "apps\LogGenerator\loggenerator-server\target\loggenerator-server-%VERSION%.jar" "." /Y
+xcopy "apps\LogGenerator\loggenerator-client\target\loggenerator-client-%VERSION%.jar" "." /Y
 
 REM Cd back to scripts
-cd ../../resources/scripts
+cd resources/scripts
+
+timeout 5
--- resources/scripts/start_loggenerator.bat
+++ resources/scripts/start_loggenerator.bat
@@ -8,15 +8,14 @@ REM Cd to root of Elasticfantastic
 
 cd ../../
 
-REM if not exist apps\LogGenerator_system mkdir apps\LogGenerator_system
-
-xcopy "apps\LogGenerator\loggenerator-server\target\loggenerator-server-%VERSION%.jar" "." /Y
-xcopy "apps\LogGenerator\loggenerator-client\target\loggenerator-client-%VERSION%.jar" "." /Y
-
-REM cd apps/LogGenerator_system
+echo Don't close this window; it will close itself when the startup process is completed.
+echo\
+echo Starting integration...
 
 start cmd /k "java -jar loggenerator-server-%VERSION%.jar Server1 resources/logs/log_server1.txt"
-timeout /t 15 /nobreak > NUL
-start cmd /k "java -jar loggenerator-client-%VERSION%.jar Client1 resources/logs/log_client1.txt"
+echo - Server1 started [1/2]
 
-REM cd ../../scripts
+timeout /t 15 /nobreak > NUL
+
+start cmd /k "java -jar loggenerator-client-%VERSION%.jar Client1 resources/logs/log_client1.txt"
+echo - Client1 started [2/2]
